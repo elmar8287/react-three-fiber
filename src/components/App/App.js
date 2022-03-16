94c858b9d398ecdbce089fe85733e42d6cac999e
Elmar <L-mar@inbox.ru>
2022-03-16 11:52:20 +0400

    Setup React-Three-Fiber

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navigation from './components/Navigation/Navigation';
+import Navigation from '../Navigation/Navigation';
+import './App.css';
 
 function App() {
   return (
